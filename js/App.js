c5e9f5223b8a805a1ff7f0126dc644fb34af4c20
Jere Vaara <jerevaara@gmail.com>
2017-02-21 20:12:57 +0200

    moved app component to App.js

---
+++ js/App.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react'
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native'
+
+export default class Spotily extends Component {
+  constructor() {
+    super()
+  }
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Welcome to Spotily!
+        </Text>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#010d06',
+  },
+  welcome: {
+    fontSize: 30,
+    textAlign: 'center',
+    fontWeight: '400',
+    color: '#0be573',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+})
+
+AppRegistry.registerComponent('Spotily', () => Spotily)
