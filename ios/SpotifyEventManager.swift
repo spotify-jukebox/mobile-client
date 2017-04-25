fff82d711e0dd7d06a7520290b9fd5addec25078
ollir <olli.rauramo@aalto.fi>
2017-04-26 01:05:32 +0300

    add an event for didStopPlayingTrack

--- ios/SpotifyEventManager.swift
+++ ios/SpotifyEventManager.swift
@@ -11,30 +11,35 @@ import Foundation
 
 @objc(SpotifyEventManager)
 class SpotifyEventManager: RCTEventEmitter, SPTAudioStreamingDelegate, SPTAudioStreamingPlaybackDelegate {
-  
+
   var player: SPTAudioStreamingController?
-  
+
   override init() {
     super.init()
-    
+
     player = SPTAudioStreamingController.sharedInstance()
     player?.playbackDelegate = self
   }
-  
+
   override func supportedEvents() -> [String]! {
     return ["audioStreamingDidChangeToTrack",
             "audioStreamingDidStartPlayingTrack",
-            "audioStreamingDidSkipToNextTrack"]
+            "audioStreamingDidSkipToNextTrack",
+            "audioStreamingDidStopPlayingTrack",]
   }
-  
+
   func audioStreamingDidSkip(toNextTrack audioStreaming: SPTAudioStreamingController!) {
     print("EVENT - audioStreamingDidSkipToNextTrack")
   }
-  
+
   func audioStreaming(_ audioStreaming: SPTAudioStreamingController!, didStartPlayingTrack trackUri: URL!) {
     print("EVENT - didStartPlayingTrack")
   }
-  
+
+  func audioStreaming(_ audioStreaming: SPTAudioStreamingController!, didStopPlayingTrack trackUri: URL!) {
+    self.sendEvent(withName: "audioStreamingDidStopPlayingTrack", body: trackUri)
+  }
+
   func audioStreaming(_ audioStreaming: SPTAudioStreamingController, didChangeToTrack metadata: [AnyHashable : Any]) {
     self.sendEvent(withName: "audioStreamingDidChangeToTrack", body: metadata)
   }
